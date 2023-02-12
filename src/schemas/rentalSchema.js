d84ed68ee23c49e859271989d3aba253a76f0a0b
lupacco <pagottolc@gmail.com>
2023-02-12 16:17:56 -0300

    updating rentals schema

--- src/schemas/rentalSchema.js
+++ src/schemas/rentalSchema.js
@@ -3,9 +3,5 @@ import Joi from "@hapi/joi";
 export const rentalSchema = Joi.object({
   customerId: Joi.number().required(),
   gameId: Joi.number().required(),
-  rentDate: Joi.string().required(),
-  daysRented: Joi.number().positive().integer().required(),
-  returnDate: Joi.string().required(),
-  originalPrice: Joi.string().required(),
-  delayFee: Joi.string().required(),
+  daysRented: Joi.number().positive().integer().required()
 });
