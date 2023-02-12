14ec1bfb543bed946582b444fe881fcd8a38287b
lupacco <pagottolc@gmail.com>
2023-02-12 18:27:51 -0300

    updating customers and games schemas

--- src/schemas/customerSchema.js
+++ src/schemas/customerSchema.js
@@ -3,6 +3,6 @@ import Joi from "@hapi/joi";
 export const customerSchema = Joi.object({
   name: Joi.string().min(1).required(),
   phone: Joi.string().required(),
-  cpf: Joi.string().min(11).required(),
+  cpf: Joi.string().pattern(/^[0-9]{11}$/).required(),
   birthday: Joi.string().required(),
 });
