8ff74a62131e9383aa8e2a26709bdccddf19c8f1
lupacco <pagottolc@gmail.com>
2023-02-12 21:10:10 -0300

    improving controllers

--- src/controllers/customers.controllers.js
+++ src/controllers/customers.controllers.js
@@ -43,7 +43,6 @@ export async function updateCustomer(req, res) {
   const newData = req.body  
   try {
     await db.query(`UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id = $5`,[newData.name, newData.phone, newData.cpf, newData.birthday, id])
-    console.log(newData)
 
     return res.sendStatus(200);
   } catch (err) {
